9ea3686311dae23d6f1a025fd620d8a97476e63b
devtrung <vuthanhtrungsuper@gmail.com>
2026-09-08 20:03:29 +0700

    feat: add mtr & gpg

--- configuration.nix
+++ configuration.nix
@@ -216,11 +216,11 @@
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
+  programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
   #   enableSSHSupport = true;
-  # };
+  };
 
   # Enable the OpenSSH daemon.
   virtualisation.docker.enable = true;
